01ed09b3636ccfb317ecb9251152a97faf0d6c50
James Grantham <jamesgrantham@192.168.1.106>
2021-01-25 14:37:04 +0000

    favicon and screen size setup

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,10 +4,14 @@ import styled from "styled-components";
 export default function Homepage() {
   const windowHeight = window.innerHeight;
 
+  window.onresize = function () {
+    window.location.reload();
+  }
+
   return (
-    <Container style={{height: `${windowHeight}px`}}>
+    <Container style={{ height: `${windowHeight}px` }}>
       <h1>intangible engineering</h1>
-      <h3>WEB DEVELOPMENT</h3>
+      <h3>WEB DEVELOPMENT SERVICE</h3>
       <a href="https://jamesgrantham.me">Visit the developer</a>
     </Container>
   );
@@ -16,18 +20,27 @@ export default function Homepage() {
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  text-align: center;
+  align-items: center;
+  /* text-align: center; */
   color: white;
   background-image: linear-gradient(to right, #376b92, #51ae59);
 
   h1 {
     margin: 20vh 0 10px 0;
-    font-size: 8vw;
+    font-size: max(7vw, 30px);
   }
 
+  h3 {
+    font-size: max(2vw, 16px);
+  }
   a {
+    text-align: center;
     margin-top: 30vh;
     text-decoration: none;
     color: white;
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 0.6rem 1.6rem;
+    /* width: 100px; */
   }
 `;
